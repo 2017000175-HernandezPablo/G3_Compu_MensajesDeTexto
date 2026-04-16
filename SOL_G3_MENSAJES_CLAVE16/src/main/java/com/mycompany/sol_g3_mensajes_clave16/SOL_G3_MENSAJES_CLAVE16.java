8fc0a2c139fa8311837fe9d50b1cb671724ba79d
2017000175-HernandezPablo <p.hernandez.5cdb@gmail.com>
2026-04-15 22:08:35 -0600

    Guia No3
    Mensajes de texto, adjunto en edoo video y rubrica

---
+++ SOL_G3_MENSAJES_CLAVE16/src/main/java/com/mycompany/sol_g3_mensajes_clave16/SOL_G3_MENSAJES_CLAVE16.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sol_g3_mensajes_clave16;
+
+/**
+ *
+ * @author Susi
+ */
+public class SOL_G3_MENSAJES_CLAVE16 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
